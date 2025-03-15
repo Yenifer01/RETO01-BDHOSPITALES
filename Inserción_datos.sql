dc2c222446b3d1b64f2c069141f00fb5aa0e1a44
Yenifer01 <yenifer.laiza@tecsup.edu.pe>
2025-03-14 23:23:45 -0500

    Scripts

---
+++ Inserción_datos.sql
@@ -0,0 +1,42 @@
+ -- INSERT PARA LA TABLA GERENTE 
+INSERT INTO GERENTE (descGerente) VALUES ('Juan Rodriguez');
+INSERT INTO GERENTE (descGerente) VALUES ('María Gómez');
+INSERT INTO GERENTE (descGerente) VALUES ('Carlos Ramírez');
+INSERT INTO GERENTE (descGerente) VALUES ('Ana López');
+INSERT INTO GERENTE (descGerente) VALUES ('Luis Fernández');
+INSERT INTO GERENTE (descGerente) VALUES ('Elena Vargas');
+
+-- INSERT PARA LA TABLA CONDICIÓN
+INSERT INTO CONDICION (descCondicion) VALUES ('Operativo');
+INSERT INTO CONDICION (descCondicion) VALUES ('En mantenimiento');
+INSERT INTO CONDICION (descCondicion) VALUES ('Cerrado temporalmente');
+INSERT INTO CONDICION (descCondicion) VALUES ('En construcción');
+INSERT INTO CONDICION (descCondicion) VALUES ('Deshabilitado');
+INSERT INTO CONDICION (descCondicion) VALUES ('Ampliación en proceso');
+
+-- INSERT PARA LA TABLA  SEDE
+INSERT INTO SEDE (descSede) VALUES ('Sede Central');
+INSERT INTO SEDE (descSede) VALUES ('Sede Norte');
+INSERT INTO SEDE (descSede) VALUES ('Sede Sur');
+INSERT INTO SEDE (descSede) VALUES ('Sede Este');
+INSERT INTO SEDE (descSede) VALUES ('Sede Oeste');
+INSERT INTO SEDE (descSede) VALUES ('Sede Metropolitana');
+
+-- INSERT PARA LA TABLA PROVINCIA 
+INSERT INTO PROVINCIA (descProvincia) VALUES ('Lima');
+INSERT INTO PROVINCIA (descProvincia) VALUES ('Arequipa');
+INSERT INTO PROVINCIA (descProvincia) VALUES ('Cusco');
+INSERT INTO PROVINCIA (descProvincia) VALUES ('Trujillo');
+INSERT INTO PROVINCIA (descProvincia) VALUES ('Piura');
+INSERT INTO PROVINCIA (descProvincia) VALUES ('Junín');
+
+-- INSERT PARA LA TABLA DISTRITO
+INSERT INTO DISTRITO (idProvincia, descDistrito) VALUES (1, 'Lince');
+INSERT INTO DISTRITO (idProvincia, descDistrito) VALUES (2, 'Cayma'); 
+INSERT INTO DISTRITO (idProvincia, descDistrito) VALUES (3, 'San Jerónimo'); 
+INSERT INTO DISTRITO (idProvincia, descDistrito) VALUES (4, 'Víctor Larco Herrera'); 
+INSERT INTO DISTRITO (idProvincia, descDistrito) VALUES (5, 'Castilla'); 
+INSERT INTO DISTRITO (idProvincia, descDistrito) VALUES (6, 'El Tambo'); 
+
+
+
---
+++ Procedimientos_almacenados.sql
@@ -0,0 +1,179 @@
+-- PROCEDIMIENTO ALMACENADO PARA  EL REGISTRO DE UN HOSPITAL 
+CREATE OR REPLACE PROCEDURE SP_HOSPITAL_REGISTRAR(
+    p_idDistrito IN NUMBER,
+    p_Nombre IN VARCHAR2,
+    p_Antiguedad IN NUMBER,
+    p_Area IN NUMBER,
+    p_idSede IN NUMBER,
+    p_idGerente IN NUMBER,
+    p_idCondicion IN NUMBER
+) IS
+    v_existe NUMBER;
+BEGIN
+    SELECT COUNT(*) 
+    INTO v_existe 
+    FROM HOSPITAL
+    WHERE nombre = p_Nombre;
+    IF v_existe > 0 THEN
+          RAISE_APPLICATION_ERROR(-20002, 'Error: Ya existe un hospital con el nombre "' || p_Nombre || '".');
+    END IF;
+    IF p_Antiguedad <= 0 OR p_Area <= 0 THEN
+         RAISE_APPLICATION_ERROR(-20003, 'Error: La antigüedad y el área deben ser mayores que 0.');
+    END IF;
+    INSERT INTO HOSPITAL (idDistrito, nombre, antiguedad, area, idSede, idGerente, idCondicion, fechaRegistro)
+    VALUES (p_idDistrito, p_Nombre, p_Antiguedad, p_Area, p_idSede, p_idGerente, p_idCondicion, SYSDATE);
+    DBMS_OUTPUT.PUT_LINE('Hospital registrado correctamente.');
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error al registrar el hospital: ' || SQLERRM);
+          RAISE; 
+END SP_HOSPITAL_REGISTRAR;
+
+-- EJECUCIÓN DEL PROCEDIMIENTO
+BEGIN
+    SP_HOSPITAL_REGISTRAR(1, 'Hospital Nacional Arzobispo Loayza', 10, 987.45, 1,1,2);
+END;
+
+
+-- PROCEDIMIENTO ALMACENADO PARA  ACTUALIZAR UN HOSPITAL 
+CREATE OR REPLACE PROCEDURE SP_HOSPITAL_ACTUALIZAR(
+    p_idHospital IN NUMBER,
+    p_idDistrito IN NUMBER,
+    p_Nombre IN VARCHAR2,
+    p_Antiguedad IN NUMBER,
+    p_Area IN NUMBER,
+    p_idSede IN NUMBER,
+    p_idGerente IN NUMBER,
+    p_idCondicion IN NUMBER
+) IS
+    v_count NUMBER;
+    v_nombre_existente NUMBER;
+    v_existente HOSPITAL%ROWTYPE; 
+    ex_no_cambio EXCEPTION; 
+BEGIN
+    SELECT COUNT(*) 
+    INTO v_count
+    FROM HOSPITAL
+    WHERE idHospital = p_idHospital;
+    IF v_count = 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'No se encontró el hospital con ID: ' || p_idHospital);
+    END IF;
+    
+    IF p_Antiguedad <= 0 OR p_Area <= 0 THEN
+        RAISE_APPLICATION_ERROR(-20002, 'Los valores de antigüedad y área deben ser mayores a 0.');
+    END IF;
+    
+    SELECT COUNT(*) 
+    INTO v_nombre_existente
+    FROM HOSPITAL
+    WHERE Nombre = p_Nombre AND idHospital <> p_idHospital;
+
+    IF v_nombre_existente > 0 THEN
+        RAISE_APPLICATION_ERROR(-20003, 'Ya existe un hospital con el nombre "' || p_Nombre || '".');
+    END IF;
+
+    SELECT * INTO v_existente FROM HOSPITAL WHERE idHospital = p_idHospital;
+    IF v_existente.idDistrito = p_idDistrito 
+       AND v_existente.Nombre = p_Nombre 
+       AND v_existente.Antiguedad = p_Antiguedad
+       AND v_existente.Area = p_Area 
+       AND v_existente.idSede = p_idSede 
+       AND v_existente.idGerente = p_idGerente 
+       AND v_existente.idCondicion = p_idCondicion THEN
+        RAISE ex_no_cambio;  
+    END IF;
+    UPDATE HOSPITAL
+    SET idDistrito = p_idDistrito, 
+        Nombre = p_Nombre, 
+        Antiguedad = p_Antiguedad,
+        Area = p_Area, 
+        idSede = p_idSede, 
+        idGerente = p_idGerente, 
+        idCondicion = p_idCondicion
+    WHERE idHospital = p_idHospital;
+    DBMS_OUTPUT.PUT_LINE('Hospital actualizado correctamente.');
+EXCEPTION
+    WHEN ex_no_cambio THEN
+        DBMS_OUTPUT.PUT_LINE('No se ha realizado ningún cambio en los datos.');
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error al actualizar el hospital: ' || SQLERRM);
+END SP_HOSPITAL_ACTUALIZAR;
+
+-- EJECUCIÓN DEL PROCEDIMIENTO
+BEGIN
+    SP_HOSPITAL_ACTUALIZAR(1,1, 'Hospital Nacional Arzobispo Loayza', 15, 987.70, 1,1,2);
+END;
+
+
+-- PROCEDIMIENTO ALMACENADO PARA ELIMINAR UN  HOSPITAL 
+CREATE OR REPLACE PROCEDURE SP_HOSPITAL_ELIMINAR(
+    p_idHospital IN NUMBER
+) IS
+    v_count NUMBER;
+BEGIN
+    SELECT COUNT(*) 
+    INTO v_count 
+    FROM HOSPITAL
+    WHERE idHospital = p_idHospital;
+    IF v_count = 0 THEN
+        DBMS_OUTPUT.PUT_LINE('Error: No se encontró el hospital con ID ' || p_idHospital || '.');
+    ELSE
+        DELETE FROM HOSPITAL WHERE idHospital = p_idHospital;
+        COMMIT;
+        DBMS_OUTPUT.PUT_LINE('El Hospital con ID ' || p_idHospital || ' fue eliminado correctamente.');
+    END IF;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error al eliminar el hospital: ' || SQLERRM);
+        ROLLBACK;
+END SP_HOSPITAL_ELIMINAR;
+
+-- EJECUCIÓN DEL PROCEDIMIENTO
+BEGIN
+    SP_HOSPITAL_ELIMINAR(3);
+END;
+
+-- PROCEDIMIENTO ALMACENADO PARA LISTAR HOSPITALES
+CREATE OR REPLACE PROCEDURE SP_HOSPITAL_LISTAR (
+    p_NomHospital IN HOSPITAL.Nombre%TYPE DEFAULT NULL,
+    p_descDistrito IN DISTRITO.descDistrito%TYPE DEFAULT NULL,
+    p_descCondicion IN CONDICION.descCondicion%TYPE DEFAULT NULL
+) IS
+    CURSOR c_hospital IS 
+        SELECT h.idHospital, d.descDistrito, h.Nombre, h.Antiguedad, h.Area, 
+               s.descSede, g.descGerente, c.descCondicion, h.fechaRegistro
+        FROM HOSPITAL h
+        INNER JOIN SEDE s ON h.idSede = s.idSede
+        INNER JOIN DISTRITO d ON h.idDistrito = d.idDistrito
+        INNER JOIN GERENTE g ON h.idGerente = g.idGerente
+        INNER JOIN CONDICION c ON h.idCondicion = c.idCondicion
+        WHERE (p_NomHospital IS NULL OR UPPER(h.Nombre) LIKE '%' || UPPER(p_NomHospital) || '%')
+          AND (p_descDistrito  IS NULL OR UPPER(d.descDistrito) LIKE '%' || UPPER(p_descDistrito) || '%')
+          AND (p_descCondicion IS NULL OR UPPER(c.descCondicion) LIKE '%' || UPPER(p_descCondicion) || '%');
+    v_count NUMBER := 0; 
+BEGIN
+    FOR v_hospital IN c_hospital LOOP
+        v_count := v_count + 1; 
+        DBMS_OUTPUT.PUT_LINE('ID: ' || v_hospital.idHospital || 
+                             ' | Nombre: ' || v_hospital.Nombre || 
+                             ' | Gerente: ' || v_hospital.descGerente || 
+                             ' | Condición: ' || v_hospital.descCondicion || 
+                             ' | Sede: ' || v_hospital.descSede || 
+                             ' | Distrito: ' || v_hospital.descDistrito);
+    END LOOP;
+    IF v_count = 0 THEN
+        DBMS_OUTPUT.PUT_LINE('No se encontraron hospitales con los datos ingresados.');
+    END IF;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error al listar los hospitales: ' || SQLERRM);
+END SP_HOSPITAL_LISTAR;
+
+-- EJECUCIÓN DEL PROCEDIMIENTO
+BEGIN
+    SP_HOSPITAL_LISTAR('hospital regional' , 'san jerónimo', null);
+END;
+
+
+
+
